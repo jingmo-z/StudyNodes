49e3d6b847491e85b953fd43c6d53b5f899a96b0
zhangpy <13298194366@163.com>
2022-01-10 15:40:36 +0800

    快速排序--lomuto分区方案和霍尔分区方案

---
+++ src/main/java/com/zpy/algorithm/sort/BilateralQuickSort.java
@@ -0,0 +1,75 @@
+package com.zpy.algorithm.sort;
+
+import com.zpy.algorithm.utils.Util;
+
+import java.util.Arrays;
+
+/**
+ * 双边循环快排
+ *
+ * @author zpy
+ * @since 2022/01/10 15:18
+ */
+public class BilateralQuickSort {
+
+    public static void main(String[] args) {
+        int[] a = {5, 3, 7, 2, 9, 8, 1, 4};
+        System.out.println(Arrays.toString(a));
+        quickSort(a, 0, a.length - 1);
+    }
+
+    /**
+     * 递归实现双边循环快排
+     *
+     * @param a 待排序数组
+     * @param l 左边界
+     * @param r 右边界
+     * @author zpy
+     * @since 2022/1/10 15:20
+     **/
+    private static void quickSort(int[] a, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int p = partition(a, l, r);
+        quickSort(a, l, p - 1);
+        quickSort(a, p + 1, r);
+    }
+
+    /**
+     * 霍尔分区实现
+     *
+     * @param a 待排序数组
+     * @param l 左边界
+     * @param r 右边界
+     * @return {@link int}
+     * @author zpy
+     * @since 2022/1/10 15:22
+     **/
+    private static int partition(int[] a, int l, int r) {
+        // 选取最左侧元素作为基准点
+        int pv = a[l];
+        // i 从左向右搜索比基准点元素大的元素
+        int i = l;
+        // j 从右向左搜索比基准点元素小的元素
+        int j = r;
+
+        while (i < j) {
+            // 从右向左搜索比基准点元素小的元素
+            while (i < j && a[j] > pv) {
+                j--;
+            }
+            // i 从左向右搜索比基准点元素大的元素
+            while (i < j && a[i] <= pv) {
+                i++;
+            }
+            Util.swap(a, i, j);
+        }
+        // 分区结束，与基准点元素进行交换
+        Util.swap(a, i, l);
+        System.out.println(Arrays.toString(a));
+        return i;
+    }
+
+
+}
---
+++ src/main/java/com/zpy/algorithm/sort/UnilateralQuickSort.java
@@ -0,0 +1,65 @@
+package com.zpy.algorithm.sort;
+
+import com.zpy.algorithm.utils.Util;
+
+import java.util.Arrays;
+
+/**
+ * 单边循环快排
+ *
+ * @author zpy
+ * @since 2022/01/09 19:46
+ */
+public class UnilateralQuickSort {
+
+    public static void main(String[] args) {
+        int[] a = {5, 3, 7, 2, 9, 8, 1, 4};
+        quickSort(a, 0, a.length - 1);
+    }
+
+    /**
+     * 递归完成单边快排
+     *
+     * @param a 待排序的数组
+     * @param l 左边界
+     * @param r 右边界
+     * @author zpy
+     * @since 2022/1/9 20:00
+     **/
+    private static void quickSort(int[] a, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int p = partition(a, l, r);
+        quickSort(a, l, p - 1);
+        quickSort(a, p + 1, r);
+    }
+
+    /**
+     * 进行一次分区操作
+     *
+     * @param a 待排序的数组
+     * @param l 左边界
+     * @param r 右边界
+     * @return {@link int} 本次分区完成的基准点的索引
+     * @author zpy
+     * @since 2022/1/9 19:54
+     **/
+    private static int partition(int[] a, int l, int r) {
+        // 基准点元素
+        int pv = a[r];
+        int i = l;
+        for (int j = l; j < r; j++) {
+            // 找到比基准点元素小的值时交换 i 和 j 索引的值
+            if (a[j] < pv) {
+                Util.swap(a, i, j);
+                i++;
+            }
+        }
+        // 将基准点元素交换到索引为 i 的位置
+        Util.swap(a, i, r);
+        System.out.println(Arrays.toString(a) + " , i = " + i);
+
+        return i;
+    }
+}
